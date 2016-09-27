55d0b674a871c5cf0bd526b0adaf8c95ebca8a36
Daniel Belisle <daniel.belisle@aofl.com>
2016-09-26 17:01:59 -0700

    solve 57

---
+++ 122/122.js
@@ -0,0 +1,1 @@
+// IDEA 1: Find chain of doubles starting at prime that gets you closest to the number
--- 57/57.rb
+++ 57/57.rb
@@ -1,8 +1,20 @@
-nk = 3.0
-dk = 2.0
+# Uses the recursive pattern:
+# 	d(n) = 2*d(n-1) + d(n-2)
+# 	n(n) = d(n-1)
+#
+# The numbers even when reduced become larger than JS can reliably deal with
+# so ruby was used to avoid a big num library
 
-1000.times do
-	nk += 2*dk
-	dk = nk - dk
-	puts nk
+d1 = 2
+d2 = 5
+ans = 0
+
+998.times do |i|
+	n = d2
+	d = 2 * d2 + d1
+  ans += 1 if (n+d).to_s.length > d.to_s.length
+	d1 = d2
+	d2 = d
 end
+
+puts ans
