57f8aeb0dce64f4cf29939e5be4634913d674b46
Daniel Belisle <daniel.belisle@aofl.com>
2016-09-30 18:29:27 -0700

    ..

--- 65/65.rb
+++ 65/65.rb
@@ -1,10 +1,14 @@
+# Due to the repeated fraction pattern for e
+# 1,2,1,1,4,1,1,6,1,....,1,2k,1
+# a simple recursive pattern can be used
+
 pn2 = 2
 pn1 = 3
 a = 2
 n = 0
 
 98.times do |i|
-  if(i % 3 === 0)
+  if i % 3 == 0
     n = a * pn1 + pn2
     a += 2
   else
