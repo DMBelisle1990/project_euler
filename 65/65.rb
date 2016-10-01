1701066edf07bc171d52280984a9530204e77aa5
Daniel Belisle <daniel.belisle@aofl.com>
2016-09-30 18:26:46 -0700

    solve 125 and 65

---
+++ 65/65.rb
@@ -0,0 +1,18 @@
+pn2 = 2
+pn1 = 3
+a = 2
+n = 0
+
+98.times do |i|
+  if(i % 3 === 0)
+    n = a * pn1 + pn2
+    a += 2
+  else
+    n = pn2 + pn1
+  end
+  pn2 = pn1
+  pn1 = n
+end
+puts n
+n = n.to_s.chars.map(&:to_i).inject(0, :+)
+puts n
--- 73/73.js
+++ 73/73.js
@@ -1,15 +1,12 @@
-var fractions = [];
+var ans = 0;
 var discovered = new Set();
 for(var d = 2; d <= 12000; d++) {
   for(var n = 1; n < d; n++) {
     if(n/d > 1/3 && n/d < 1/2 && !discovered.has(n/d)) {
-      fractions.push({
-        exact: [n, d],
-        estimate: n/d
-      });
+      ans++;
       discovered.add(n/d);
     }
   }
 }
 
-console.log(fractions.length);
+console.log(ans);
