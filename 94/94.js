06342f7615b3ed319ab460584715258b9036750d
Daniel Belisle <daniel.belisle@aofl.com>
2016-10-04 18:42:22 -0700

    solve 62

--- 94/94.js
+++ 94/94.js
@@ -1,45 +1,14 @@
-// var a1, a2, ans = 0;
+var ans = 0;
+for(var i = 2; i < 333333334; i++) {
+  for(var j = -1; j < 2; j+=2) {
+    var temp = Math.sqrt((3*i + j)) * Math.sqrt((i - j));
+    temp *= (i - j)/4;
+    if(temp % 1 === 0) {
+      ans += 3*i + j;
+      console.log(temp);
 
-// for(var i = 1; i < 1000; i++) {
-// 	a1 = (2 + Math.sqrt(4 + 12*(16*i*i + 1))) / 6;
-// 	a2 = (2 - Math.sqrt(4 + 12*(16*i*i + 1))) / 6;
-// 	b1 = (-2 + Math.sqrt(4 + 12*(16*i*i + 1))) / 6;
-// 	b2 = (-2 - Math.sqrt(4 + 12*(16*i*i + 1))) / 6;
-// 	if(a1%1 == 0) 
-// 		ans += (3*a1 + 1);
-// 	if(a2%1 == 0) 
-// 		ans += (3*a2 + 1);
-// 	if(b1%1 == 0) 
-// 		console.log(b1);
-// 	if(b2%1 == 0) 
-// 		console.log(b2);
-// }
+    }
+  }
+}
 
-
-// console.log(ans);
-
-// var ans = 0, count = 0, p;
-// for(var i = 2; i < 333333333; i++) {
-// 	p = (3*i + 1)/2;
-// 	if(i==5) console.log(Math.sqrt( p*2*(p-i)*(p-i-1) ));
-// 	if(Math.sqrt( p*2*(p-i)*(p-i-1) )%1 == 0) {
-// 		console.log(i);
-// 		// console.log(Math.sqrt( p*2*(p-i)*(p-i-1) ));
-// 		ans += (2*p);
-// 	}
-// 	p = (3*i - 1)/2;
-// 	if(Math.sqrt( p*2*(p-i)*(p-i+1) )%1 == 0) {
-// 		console.log(i);
-// 		ans += (2*p);
-// 	}
-
-// 	if(i==5) {
-// 	 console.log(Math.sqrt( p*2*(p-i)*(p-i+1) ));
-// 	 break;
-// 	}
-// }
-
-// console.log(ans);
-
-for(var i = 0; i < 333333333; i++) {} console.log("done");
-
+console.log(ans);
