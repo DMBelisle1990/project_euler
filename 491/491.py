0aaf24f010f907f38bec64324bb3a3c979c5aca4
Daniel Belisle <daniel.belisle@aofl.com>
2017-05-17 18:29:38 -0700

    solve 581

---
+++ 491/491.py
@@ -0,0 +1,11 @@
+# There are only 5 possible sums -44, -22, 0, 22, 44
+# -22 and -44 probably are the same number of numbers as 22 and 44 so that leaves 3 left to focus on
+
+# Were assigning numbers to 2 arrays, an plus and minus array
+# Once a valid assignment is found we take all same size combinations of that array
+# 0 has 1 solution
+# 44:
+# x - y = 44
+# x + y = 90
+# x = 67, y = 23
+# Need to find total possible way to sum 10 numbers to 23 
--- 581/581.py
+++ 581/581.py
@@ -15,24 +15,47 @@ def powerset(iterable):
     xs = list(iterable)
     return chain.from_iterable(combinations(xs,n) for n in range(len(xs)+1))
 
-prime_sets = powerset([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47])
-q = [reduce(mul, s, 1) for s in prime_sets]
-q.remove(2)
 
 def pell(d):
     """ Returns fundamental solution (x1,y1) for pell equation with nonsquare d """
-    p, k, x1, y, sd = 1, 1, 1, 0, d ** 0.5
+    p, k, x1, y, sd = 1, 1, 1, 0, d**0.5
     if sd == int(sd):
         return 0
     while k != 1 or y == 0:
         p = k * (p/k+1) - p
         p = p - int((p - sd)/k) * k
- 
         x = (p*x1 + d*y) / abs(k)
         y = (p*y + x1) / abs(k)
         k = (p*p - d) / k
         x1 = x
     return x, y
 
-print pell(109)
+primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]
+max_n = 10 ** 12
+
+def is_47_smooth(n):
+    if n > max_n:
+        return False
+    for p in primes:
+        while n % p == 0:
+            n //= p
+    return n < 48
+
+prime_sets = powerset(primes)
+pell_qs = [reduce(mul, s, 1) for s in prime_sets] # product of each subset of primes
+pell_qs.remove(2)
+ans = 0
+for q in pell_qs:
+    x1, y1 = pell(2*q) # This is the main bottleneck
+    x, y = [x1, x1**2 + 2*q * y1**2], [y1, 2*x1*y1]
+    for i in range(2, 24):
+        xk1 = x1*x[-1] + 2*q * y1*y[-1]
+        yk1 = x1*y[-1] + y1*x[-1]
+        x.append(xk1)
+        y.append(yk1)
+    for n in x:
+        if is_47_smooth((n-1)/2) and is_47_smooth((n+1)/2):
+            ans += (n-1)/2
+
+print ans
      
